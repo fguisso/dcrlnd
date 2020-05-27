lightningnetwork/lnd

config+lncfg: move config structs to lncfg, drop suffix

lncfg/btcd.go
@@ -1,0 +1,11 @@
+package lncfg
+
+// Btcd holds the configuration options for the daemon's connection to btcd.
+type Btcd struct {
+	Dir        string `long:"dir" description:"The base directory that contains the node's data, logs, configuration file, etc."`
+	RPCHost    string `long:"rpchost" description:"The daemon's rpc listening address. If a port is omitted, then the default port for the selected chain parameters will be used."`
+	RPCUser    string `long:"rpcuser" description:"Username for RPC connections"`
+	RPCPass    string `long:"rpcpass" default-mask:"-" description:"Password for RPC connections"`
+	RPCCert    string `long:"rpccert" description:"File containing the daemon's certificate file"`
+	RawRPCCert string `long:"rawrpccert" description:"The raw bytes of the daemon's PEM-encoded certificate chain which will be used to authenticate the RPC connection."`
+}

lncfg/chain.go
@@ -1,0 +1,24 @@
+package lncfg
+
+import "github.com/lightningnetwork/lnd/lnwire"
+
+// Chain holds the configuration options for the daemon's chain settings.
+type Chain struct {
+	Active   bool   `long:"active" description:"If the chain should be active or not."`
+	ChainDir string `long:"chaindir" description:"The directory to store the chain's data within."`
+
+	Node string `long:"node" description:"The blockchain interface to use." choice:"btcd" choice:"bitcoind" choice:"neutrino" choice:"ltcd" choice:"litecoind"`
+
+	MainNet  bool `long:"mainnet" description:"Use the main network"`
+	TestNet3 bool `long:"testnet" description:"Use the test network"`
+	SimNet   bool `long:"simnet" description:"Use the simulation test network"`
+	RegTest  bool `long:"regtest" description:"Use the regression test network"`
+
+	DefaultNumChanConfs int                 `long:"defaultchanconfs" description:"The default number of confirmations a channel must have before it's considered open. If this is not set, we will scale the value according to the channel size."`
+	DefaultRemoteDelay  int                 `long:"defaultremotedelay" description:"The default number of blocks we will require our channel counterparty to wait before accessing its funds in case of unilateral close. If this is not set, we will scale the value according to the channel size."`
+	MinHTLCIn           lnwire.MilliSatoshi `long:"minhtlc" description:"The smallest HTLC we are willing to accept on our channels, in millisatoshi"`
+	MinHTLCOut          lnwire.MilliSatoshi `long:"minhtlcout" description:"The smallest HTLC we are willing to send out on our channels, in millisatoshi"`
+	BaseFee             lnwire.MilliSatoshi `long:"basefee" description:"The base fee in millisatoshi we will charge for forwarding payments on our channels"`
+	FeeRate             lnwire.MilliSatoshi `long:"feerate" description:"The fee rate used when forwarding payments on our channels. The total fee charged is basefee + (amount * feerate / 1000000), where amount is the forwarded amount."`
+	TimeLockDelta       uint32              `long:"timelockdelta" description:"The CLTV delta we will subtract from a forwarded HTLC's timelock value"`
+}

pilot.go
@@ -9,6 +9,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/autopilot"
+	"github.com/lightningnetwork/lnd/lncfg"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/tor"
 )
@@ -17,7 +18,7 @@ import (
 // configuration is sane. Currently it checks that the heuristic configuration
 // makes sense. In case the config is valid, it will return a list of
 // WeightedHeuristics that can be combined for use with the autopilot agent.
-func validateAtplCfg(cfg *autoPilotConfig) ([]*autopilot.WeightedHeuristic,
+func validateAtplCfg(cfg *lncfg.AutoPilot) ([]*autopilot.WeightedHeuristic,
 	error) {
 
 	var (
@@ -138,8 +139,8 @@ var _ autopilot.ChannelController = (*chanController)(nil)
 // Agent instance based on the passed configuration structs. The agent and all
 // interfaces needed to drive it won't be launched before the Manager's
 // StartAgent method is called.
-func initAutoPilot(svr *server, cfg *autoPilotConfig, chainCfg *chainConfig) (
-	*autopilot.ManagerCfg, error) {
+func initAutoPilot(svr *server, cfg *lncfg.AutoPilot,
+	chainCfg *lncfg.Chain) (*autopilot.ManagerCfg, error) {
 
 	atplLog.Infof("Instantiating autopilot with active=%v, "+
 		"max_channels=%d, allocation=%f, min_chan_size=%d, "+

fguisso/dcrlnd

lncfg/btcd.go
@@ -1,0 +1,10 @@
+package lncfg
+
+type DcrdConfig struct {
+	Dir        string `long:"dir" description:"The base directory that contains the node's data, logs, configuration file, etc."`
+	RPCHost    string `long:"rpchost" description:"The daemon's rpc listening address. If a port is omitted, then the default port for the selected chain parameters will be used."`
+	RPCUser    string `long:"rpcuser" description:"Username for RPC connections"`
+	RPCPass    string `long:"rpcpass" default-mask:"-" description:"Password for RPC connections"`
+	RPCCert    string `long:"rpccert" description:"File containing the daemon's certificate file"`
+	RawRPCCert string `long:"rawrpccert" description:"The raw bytes of the daemon's PEM-encoded certificate chain which will be used to authenticate the RPC connection."`
+}

lncfg/chain.go
@@ -1,0 +1,24 @@
+package lncfg
+
+import "github.com/decred/dcrlnd/lnwire"
+
+// Chain holds the configuration options for the daemon's chain settings.
+type Chain struct {
+	Active   bool   `long:"active" description:"If the chain should be active or not."`
+	ChainDir string `long:"chaindir" description:"The directory to store the chain's data within."`
+
+	Node string `long:"node" description:"The blockchain interface to use." choice:"btcd" choice:"bitcoind" choice:"neutrino" choice:"ltcd" choice:"litecoind"`
+
+	MainNet  bool `long:"mainnet" description:"Use the main network"`
+	TestNet3 bool `long:"testnet" description:"Use the test network"`
+	SimNet   bool `long:"simnet" description:"Use the simulation test network"`
+	RegTest  bool `long:"regtest" description:"Use the regression test network"`
+
+	DefaultNumChanConfs int                 `long:"defaultchanconfs" description:"The default number of confirmations a channel must have before it's considered open. If this is not set, we will scale the value according to the channel size."`
+	DefaultRemoteDelay  int                 `long:"defaultremotedelay" description:"The default number of blocks we will require our channel counterparty to wait before accessing its funds in case of unilateral close. If this is not set, we will scale the value according to the channel size."`
+	MinHTLCIn           lnwire.MilliAtom `long:"minhtlc" description:"The smallest HTLC we are willing to accept on our channels, in millisatoshi"`
+	MinHTLCOut          lnwire.MilliAtom `long:"minhtlcout" description:"The smallest HTLC we are willing to send out on our channels, in millisatoshi"`
+	BaseFee             lnwire.MilliAtom `long:"basefee" description:"The base fee in millisatoshi we will charge for forwarding payments on our channels"`
+	FeeRate             lnwire.MilliAtom `long:"feerate" description:"The fee rate used when forwarding payments on our channels. The total fee charged is basefee + (amount * feerate / 1000000), where amount is the forwarded amount."`
+	TimeLockDelta       uint32              `long:"timelockdelta" description:"The CLTV delta we will subtract from a forwarded HTLC's timelock value"`
+}

pilot.go
@@ -9,6 +9,7 @@ import (
 	"github.com/decred/dcrd/dcrutil/v2"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/autopilot"
+	"github.com/decred/dcrlnd/lncfg"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/tor"
 )
@@ -17,7 +18,7 @@ import (
 // configuration is sane. Currently it checks that the heuristic configuration
 // makes sense. In case the config is valid, it will return a list of
 // WeightedHeuristics that can be combined for use with the autopilot agent.
-func validateAtplCfg(cfg *autoPilotConfig) ([]*autopilot.WeightedHeuristic,
+func validateAtplCfg(cfg *lncfg.AutoPilot) ([]*autopilot.WeightedHeuristic,
 	error) {
 
 	var (
@@ -138,8 +139,8 @@ var _ autopilot.ChannelController = (*chanController)(nil)
 // Agent instance based on the passed configuration structs. The agent and all
 // interfaces needed to drive it won't be launched before the Manager's
 // StartAgent method is called.
-func initAutoPilot(svr *server, cfg *autoPilotConfig, c *Config) (
-	*autopilot.ManagerCfg, error) {
+func initAutoPilot(svr *server, cfg *lncfg.AutoPilot,
+	c *Config) (*autopilot.ManagerCfg, error) {
 
 	atplLog.Infof("Instantiating autopilot with active=%v, "+
 		"max_channels=%d, allocation=%f, min_chan_size=%d, "+